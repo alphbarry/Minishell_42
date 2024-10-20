54b410f8a1346628fbd2beb0ebb5035fd79e15b3
alphbarr <alphaomar9@gmail.com>
2024-10-20 20:16:39 +0200

    makefile y metacaracteres definidos

--- objects/main.d
+++
@@ -1,7 +0,0 @@
-objects/main.o: src/main.c src/../inc/minishell.h \
- src/../inc/./libft/src/libft.h src/../inc/./libft/src/get_next_line.h \
- src/../inc/./libft/src/libft.h
-src/../inc/minishell.h:
-src/../inc/./libft/src/libft.h:
-src/../inc/./libft/src/get_next_line.h:
-src/../inc/./libft/src/libft.h:
